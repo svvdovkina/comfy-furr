db63be596fe50a6f2c47122a0d5d94d0f450589f
Svetlana Vdovkina <svvdovkina@gmail.com>
2023-07-11 17:28:34 +0300

    finish layout components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 
+import {About, Cart, Checkout, Home, Products, SingleProduct, Error} from "./pages"
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <>
+      <Navbar/>
+      <Sidebar/>
+      <Outlet/>
+      <Footer/>
+    </>,
+    errorElement: <Error/>,
+    children:
+    [
+      {
+        index: true,
+        element: <Home/>
+      },
+      {
+        path: '/about',
+        element: <About/>
+      },
+      {
+        path: '/cart',
+        element: <Cart/>
+      },
+      {
+        path: '/products',
+        element: <Products/>
+      },
+      {
+        path: '/checkout',
+        element: <Checkout/>
+      },
+      {
+        path: '/products/:id',
+        element: <SingleProduct/>
+      }
+    ]
+  }
+])
+
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return <RouterProvider router={router}/>
+  
 }
 
 export default App
