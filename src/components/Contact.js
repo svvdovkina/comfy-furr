e404102524f3c7d81270a9ad2b3e36d97d3ca11c
Svetlana Vdovkina <svvdovkina@gmail.com>
2023-07-17 16:16:42 +0300

    add Hero, Services and Contact components to the home page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,18 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return <Wrapper>
+    <div className="section-center">
+      <h3>Join our newsletter and get 20% off</h3>
+      <div className="content">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt ab modi numquam, vitae sit expedita facere aspernatur sapiente quae natus.</p>
+        <form action="" className="contect-form">
+          <input type="email" name="" className='form-input' placeholder='enter-email' />
+          <button type="submit" className='submit-btn'>submit</button>
+        </form>
+      </div>
+    </div>
+  </Wrapper>
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
