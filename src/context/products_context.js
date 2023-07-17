875e4074d4f5fe89c71c8c8048e1f8a07c6f304f
Svetlana Vdovkina <svvdovkina@gmail.com>
2023-07-17 17:56:02 +0300

    add single product context

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -18,7 +18,10 @@ const initialState = {
   products_loading: false,
   products_error: false,
   products: [],
-  featured_products: []
+  featured_products: [],
+  single_product_loading: false,
+  single_product_error: false,
+  single_product: {},
 }
 
 const ProductsContext = React.createContext()
@@ -48,11 +51,25 @@ export const ProductsProvider = ({ children }) => {
         type:GET_PRODUCTS_SUCCESS,
         payload: products
       });
-      console.log(products)
 
     } catch (error) {
-      dispatch({type: GET_PRODUCTS_ERROR, payload: {error}})
+      dispatch({type: GET_PRODUCTS_ERROR})
     }
+  }
+
+  const fetchSingleProduct = async (url)=>{
+    dispatch({type: GET_SINGLE_PRODUCT_BEGIN});
+    try {
+      const data = await axios.get(url);
+      const product = data.data;
+      dispatch({
+        type:GET_SINGLE_PRODUCT_SUCCESS,
+        payload: product
+      });
+    } catch (error) {
+      dispatch({type: GET_SINGLE_PRODUCT_ERROR})
+    }
+
   }
 
   useEffect(()=>{
@@ -65,7 +82,8 @@ export const ProductsProvider = ({ children }) => {
     <ProductsContext.Provider value={
       {...state, 
       openSidebar,
-      closeSidebar
+      closeSidebar,
+      fetchSingleProduct
       }
       }>
       {children}
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -17,6 +17,7 @@ const products_reducer = (state, action) => {
   if (action.type === SIDEBAR_CLOSE) {
     return {...state, isSidebarOpen: false}
   }
+
   if (action.type === GET_PRODUCTS_BEGIN){
     return {...state, products_loading: true}
   }
@@ -35,6 +36,29 @@ const products_reducer = (state, action) => {
       ...state, 
       products_loading: false,
       products_error: true
+    }
+  }
+
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return {
+      ...state, 
+      single_product_loading: true,
+      single_product_error: false
+    }
+  }
+  if (action.type === GET_SINGLE_PRODUCT_SUCCESS){
+    const product = action.payload;
+    return {
+      ...state, 
+      products_loading: false,
+      single_product: product
+    }
+  }
+  if (action.type === GET_SINGLE_PRODUCT_ERROR){
+    return {
+      ...state, 
+      single_product_loading: false,
+      single_product_error: true
     }
   }
   throw new Error(`No Matching "${action.type}" - action type`)
