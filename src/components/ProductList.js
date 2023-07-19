068fab817f9877390edd77a5e6f41f44eb31b3f9
Svetlana Vdovkina <svvdovkina@gmail.com>
2023-07-19 15:12:31 +0300

    set products part of products page

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,7 +4,17 @@ import GridView from './GridView'
 import ListView from './ListView'
 
 const ProductList = () => {
-  return <h4>product list</h4>
+  const {filtered_products, grid_view} = useFilterContext();
+
+  if (filtered_products.length < 1) {
+    return <h5 style={{textTransform: 'none'}}>
+      Sorry, no products matched your search...
+    </h5>
+  }
+
+  if (grid_view) return <GridView products={filtered_products}/>
+
+  return <ListView products={filtered_products}/>
 }
 
 export default ProductList
